62b2aa4a7cfc88392f6b552f8985e5f70da9f4e6
James Halliday <mail@substack.net>
2013-03-28 03:52:05 -0700

    usage

--- bin/cmd.js
+++ bin/cmd.js
@@ -4,16 +4,23 @@ var shoe = require('shoe');
 var http = require('http');
 var shux = require('shux')();
 var spawn = require('child_process').spawn;
-var argv = require('optimist').argv;
 var os = require('os');
 var path = require('path');
+var fs = require('fs');
+
 var through = require('through');
 var duplexer = require('duplexer');
 var hyperquest = require('hyperquest');
 
+var argv = require('optimist').argv;
+
 if (argv._[0] === 'show') {
     var args = [ __dirname + '/show.js' ].concat(process.argv.slice(3));
     return spawn(process.execPath, args, { stdio: 'inherit' });
+}
+
+if (argv.h || argv.help || argv._[0] === 'help') {
+    return fs.createReadStream(__dirname + '/usage.txt').pipe(process.stdout);
 }
 
 if (argv.viewer && argv.app === undefined) argv.app = true;
--- readme.markdown
+++ readme.markdown
@@ -12,6 +12,31 @@ command:
 
 ![exterminate](http://substack.net/images/screenshots/exterminate.png)
 
+# usage
+
+```
+usage:
+
+  exterminate OPTIONS
+  
+    Create an exterminate session or server.
+    
+    OPTIONS:
+    
+      --port=PORT   Listen on 0.0.0.0:PORT and go into server mode.
+      
+      --viewer      Broadcast the first connection to all later connections.
+      
+      --app=CMD     Command to launch `google-chrome` as or false to not launch.
+
+  exterminate show FILE
+  
+    Render the html, image, or text content at FILE inline in the terminal.
+    
+    This command is also available as `xtshow`.
+
+```
+
 # install
 
 First install google chrome ane make sure `google-chrome` is in your `$PATH`.
